55ea0628873e6bf791b1250e97de857aa49366b0
GARTU <juan.daniel.rangel.avila@gmail.com>
2023-10-11 18:33:01 +0000

    fribo

---
+++ Fribo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#by juan.daniel.rangel.avilaq@gmail.com
+#GNU/GPL License
+#My first secuence
+
+#fribonacci secuense
+#a b c
+# 1, 1, 2, 3, 5, 8, 13, 21
+N=10
+a=1
+b=1
+c=0
+print(a)
+print(b)
+for x in range(N+1):
+	c=a+b
+	b=a
+	a=c
+	print(c)
+
